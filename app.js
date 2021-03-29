b4c51b101ddfc81b5c3f50bc9d846ed9eb001ae3
Debargho Basak <54078682+Debargho99@users.noreply.github.com>
2021-03-30 01:32:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/",function(req,res){
+   res.sendFile( __dirname + "/signup.html");
+});
+
+
+app.post("/",function(req, res){
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email= req.body.email;
+
+  console.log(firstName,lastName,email);
+
+   const data = {
+     members: [
+       {
+         email_address: email,
+         status: "subscribed",
+         merge_fields: {
+           FNAME: firstName,
+           LNAME: lastName
+         }
+
+       }
+     ]
+   }
+
+   const jsonData = JSON.stringify(data);
+
+   const url = "https://us1.api.mailchimp.com/3.0/lists/a93ba43a44";
+
+   const options = {
+     method: "POST",
+     auth: "debargho1:e5e6b33b34c8333cff568bb9ddefc094-us1"
+   }
+
+   const request = https.request(url, options, function(response) {
+        if(response.statusCode === 200)
+        {
+          res.sendFile(__dirname + "/success.html");
+        }
+
+        else
+        {
+          res.sendFile(__dirname + "/failure.html");
+        }
+
+       response.on("data", function(data) {
+       console.log(JSON.parse(data));
+     });
+   });
+
+
+ request.write(jsonData);
+ request.end();
+
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000 ,function(){
+  console.log("Server Running on port 3000");
+});
+
+// API Keys
+// e5e6b33b34c8333cff568bb9ddefc094-us1
+
+// List ID
+// a93ba43a44
